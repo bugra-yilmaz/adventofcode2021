d6537c918b91221358b072eade7ce940db413938
bugra-yilmaz <bugra.yyilmaz@gmail.com>
2021-12-15 07:06:55 +0100

    Add day01 solution

---
+++ day1/part1.py
@@ -0,0 +1,52 @@
+import os.path
+
+import pytest
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> int:
+    lines = s.splitlines()
+    numbers = [int(l) for l in lines]
+
+    count = 0
+    for i in range(0, len(lines) - 1):
+        count += numbers[i+1] > numbers[i]
+
+    return count
+
+
+INPUT_S = '''\
+199
+200
+208
+210
+200
+207
+240
+269
+260
+263
+'''
+EXPECTED = 7
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    with open(INPUT_TXT, "r") as f:
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
